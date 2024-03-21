1bedca7a0a699e1946c5013a15fd230d7bee2394
LeslT <lesliejailine@hotmail.com>
2024-03-21 18:44:50 -0500

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { menuItems } from "./data/db"
+
+function App() {
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">tip and consumption calculator</h1>
+      </header>
+
+    </>
+  )
+}
+
+export default App
